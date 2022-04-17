d114848b65cc6848fc76f3759b33a41208f12191
pipex98 <felipevasquez1998@gmail.com>
2022-04-17 11:11:56 -0500

    se agrego vendedores y una propiedad para buscar si hay stock

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { chromium } = require('playwright')
 const shops = [
     {
         vendor: 'Microsoft',
+        hasSchema: false,
         url: 'https://www.xbox.com/es-es/configure/8WJ714N3RBTL',
         checkStock: async({page}) => {
             const content = await page.textContent('[aria-label="Finalizar la compra del pack"]')
@@ -12,24 +13,54 @@ const shops = [
     },
     {
         vendor: 'Amazon',
-        url: 'https://www.amazon.es/Microsoft-RRT-00009-Xbox-Series-X/dp/B08H93ZRLL/ref=as_li_ss_tl?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2MRJRZGD1CBJ4&dchild=1&keywords=xbox+series+x&qid=1607550196&sprefix=xbox+s,aps,198&sr=8-1&linkCode=sl1&tag=todoal21-21&linkId=4e08b7956be2cf185b9b23870c56929c&language=es_ES',
+        hasSchema: false,
+        url: 'https://www.amazon.es/Far-Cry-Limited-Exclusiva-Amazon/dp/B08CHMHM9C/ref=sr_1_1?keywords=far%2Bcry%2B6%2Bps4&qid=1650122438&sprefix=far%2Bc%2Caps%2C255&sr=8-1&th=1',
         checkStock: async({ page }) => {
-            const content = await page.textContent('.a-color-price')
-            return content.includes('No disponible') === false
+            const addToCartButton = await page.$$('#add-to-cart-button')
+            return addToCartButton.length > 0
         }
     },
     {
         vendor: 'Exito',
+        hasSchema: false,
         url: 'https://www.exito.com/consola-xbox-series-x-1tb-microsoft-101154798-mp/p',
         checkStock: async({ page }) => {
             const content = await page.textContent('.exito-product-details-3-x-lastUnitsText')
             return content.includes('No disponible') === false
         }
-    }
-]
+    },
+    // {
+    //     vendor: 'Fnac',
+    //     hasSchema: true,
+    //     url: 'https://www.fnac.es/Consola-Xbox-Series-S-512GB-Blanco-Videoconsola-Consola/a7732210',
+    //     checkStock: async({page}) => {
+    //         const notAvailableIcon = await page.$$('.f-buyBox-availabilityStatus-unavailable') //estamos evaluando si existe el icono, devuelve un array
+    //         return notAvailableIcon.length === 0
+    //     }
+    // },
+//     disabled for now because it's not working property
+//     {
+//         vendor: 'Mediamarkt',
+//         hasSchema: true
+//         url: 'https://www.mediamarkt.es/es/product/_consola-microsoft-xbox-series-x-1-tb-ssd-negro-1487615.html',
+//         checkStock: async({ page }) => {
+//             const content = await page.textContent('.BaseTypo-sc-1jga2g7-0.izkVco.StyledInfoTypo-sc-1jga2g7-1.ecJNXP')
+//             return content.includes('No está disponible') === false
+//         }
+//     }
+    // {
+    //     vendor: 'El Corte Inglés',
+    //     hasSchema: false,
+    //     url: 'https://www.elcorteingles.es/videojuegos/A37047080-xbox-series-s/?color=Blanco',
+    //     checkStock: async({ page }) => {
+    //         const content = await page.textContent('.js_add_to_cart_desktop')
+    //         return content.includes('Agotado temporalmente') === false
+    //     }
+    // }
+ ]
 
 ;(async () => {
-    const browser = await chromium.launch();
+    const browser = await chromium.launch({ headless: false });
 
     for (const shop of shops) {
         const { checkStock, vendor, url } = shop
